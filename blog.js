e1b2948a216a9cb2801ae193cc8c0d95bb0db2f4
sifath islam <sifathislam790@gmail.com>
2024-01-23 18:19:30 +0600

    s

--- blog.js
+++ blog.js
@@ -16,12 +16,21 @@ const loadblog = () => {
 
         div.innerHTML=` 
         <h1>Title: ${single_data.Title}</h1>
-        <h2>Publisher Name: ${single_data.Publisher_name} </h2>
-        <p>${single_data.body}</p>
+        <h2><img src="Profile.png" class="blog-image-design"> ${single_data.Publisher_name} · November 20, 2023</h2>
+        <p>${(single_data.body).slice(0, 440)}.....</p>
+        <button onclick="ShowFullBlog(${single_data.id})" class="blog_read">Read more</button> 
         `;
         parent.appendChild(div);
     });
 
   };
 
+
+  const ShowFullBlog = (id) =>{
+  console.log(id);
+    fetch(`https://sifatislamprotfolioapi.onrender.com/blog/list/${id}`)
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err))
+  }
   loadblog()
